a2c73a3dd46e40d637a6fba98a529e3bb76dff3f
BlueBenjaminBen <benjaminle805@gmail.com>
2024-06-21 11:46:34 -0400

    handMenu UI visible
    interation and functionality with the ui is not yet working
    
    
    Former-commit-id: 7f4b8942bc9479f3aec54485f478121b6b163816

---
+++ Assets/OpenXR/HandMenu/Scripts/Hand Button Hover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandButtonHover : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        button.targetGraphic.color = button.colors.highlightedColor;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        button.targetGraphic.color = button.colors.normalColor;
+    }
+
+}
